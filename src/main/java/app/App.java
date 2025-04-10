853b19c73bb3a457a161d0a8ff4ea9fd9ada1a6b
Баратов Теймур <66258576+Teymuruae@users.noreply.github.com>
2025-04-10 20:07:32 +0300

    sout rugaga copy

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -4,5 +4,7 @@ public class App {
 
     public static void main(String[] args) {
  System.out.println("rugaga");
+         System.out.println("rugaga");
+         System.out.println("rugaga");
     }
 }
